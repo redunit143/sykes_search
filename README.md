3c7dc50d98951b5c77009f0075f24c2f0be7f926
daystrom <daystrom@post.horizon>
2021-12-09 04:09:25 +0000

    added boostrap classes in blade templates.

--- README.md
+++ README.md
@@ -1,3 +1,14 @@
+## Install
+composer install
+
+chmod 777 -R storage
+
+mv .env.example .env
+
+php artisan key:generate
+
+
+
 <p align="center"><a href="https://laravel.com" target="_blank"><img src="https://raw.githubusercontent.com/laravel/art/master/logo-lockup/5%20SVG/2%20CMYK/1%20Full%20Color/laravel-logolockup-cmyk-red.svg" width="400"></a></p>
 
 <p align="center">
---
+++ resources/views/property/part/search-form.blade.php
@@ -0,0 +1,79 @@
+<!-- Modal -->
+<div id="myModal" class="modal fade" role="dialog">
+  <div class="modal-dialog">
+
+    <!-- Modal content-->
+    <div class="modal-content">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal">&times;</button>
+        <h4 class="modal-title">Modal Header</h4>
+      </div>
+      <div class="modal-body">
+
+            <form method="POST" action="search">
+                <div class="form-group">
+                    <label>Location</label></label><input class="form-control" type="input" name="location" value="{{ old('location') ? old('location') : request('location') }}">
+                    @error('location')
+                        <span class="alert alert-danger">{{ $errors->first('location') }}</span>
+                    @enderror
+                </div>
+                <div class="checkbox">
+                    <label>Near the Beach</label> <input name="near_beach" type="checkbox" value="1" {{ ((old('near_beach') ? old('near_beach') : request('near_beach')) == 1 ? 'checked="checked"' : '') }}>
+                    @error('near_beach')
+                        <span class="alert alert-danger">{{ $errors->first('near_beach') }}</span>
+                    @enderror
+                </div>
+
+                <div class="checkbox">
+                    <label>Accepts Pets <input name="accepts_pets" type="checkbox" value="1" {{ ((old('accepts_pets') ? old('accepts_pets') : request('accepts_pets')) == 1 ? 'checked="checked"' : '') }}></label>
+                    @error('accepts_pets')
+                        <span class="alert alert-danger">{{ $errors->first('accepts_pets') }}</span>
+                    @enderror
+                </div>
+                <div class="form-group">
+                    <label>Sleeps</label></label><input class="form-control" type="input" name="sleeps" value="{{ old('sleeps') ? old('sleeps') : request('sleeps') }}">
+                    @error('sleeps')
+                        <span class="alert alert-danger">{{ $errors->first('sleeps') }}</span>
+                    @enderror
+                </div>
+                <div class="form-group">
+                    <label>Beds</label></label><input class="form-control" type="input" name="beds" value="{{ old('beds') ? old('beds') : request('beds') }}">
+                    @error('beds')
+                        <span class="alert alert-danger">{{ $errors->first('beds') }}</span>
+                    @enderror
+                </div>
+                <div class="form-group">
+                    <label>Start Date</label>
+                    <div class='input-group date'>
+                        <input type='text' class="form-control" name="start_date" value="{{ old('start_date') ? old('start_date') : request('start_date') }}" placeholder="YYYY-MM-DD" id='startdate' autocomplete="off" />
+                        <span class="input-group-addon">
+                              <span class="glyphicon glyphicon-calendar"></span>
+                        </span>
+                    </div>
+                    @error('start_date')
+                        <span class="alert alert-danger">{{ $errors->first('start_date') }}</span>
+                    @enderror
+                </div>
+                <div class="form-group">
+                    <label>End Date</label>
+                    <div class='input-group date'>
+                        <input type='text' class="form-control" name="end_date" value="{{ old('end_date') ? old('end_date') : request('end_date') }}" placeholder="YYYY-MM-DD" id='enddate' autocomplete="off" />
+                        <span class="input-group-addon">
+                              <span class="glyphicon glyphicon-calendar"></span>
+                        </span>
+                    </div>
+                    @error('end_date')
+                        <span class="alert alert-danger">{{ $errors->first('end_date') }}</span>
+                    @enderror
+                </div>
+                <button type="submit" class="btn btn-primary">Submit</button>
+            </form>
+     </div>
+
+      <div class="modal-footer">
+        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+      </div>
+    </div>
+
+  </div>
+</div>
